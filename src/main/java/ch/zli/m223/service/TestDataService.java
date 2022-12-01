d3a5d3511b7cf65383026a11e2baa2716d17e380
Norwin Schaefer <norwin.schaefer@ergon.ch>
2022-12-01 10:40:44 +0000

    feat: improved testdataservice

--- src/main/java/ch/zli/m223/service/TestDataService.java
+++ src/main/java/ch/zli/m223/service/TestDataService.java
@@ -1,10 +1,13 @@
 package ch.zli.m223.service;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import ch.zli.m223.model.Category;
@@ -17,25 +20,62 @@ import io.quarkus.runtime.StartupEvent;
 @ApplicationScoped
 public class TestDataService {
     @Inject
-    private EntryService entryService;
-
-    @Inject
-    private CategoryService categoryService;
-
-    @Inject
-    private TagService tagService;
+    EntityManager entityManager;
 
     @Transactional
-    void generateTestData(@Observes StartupEvent startupEvent) {
-        Category category1 = new Category("Category 1");
-        categoryService.createCategory(category1);
-        categoryService.createCategory(new Category("Category 2"));
+    void generateTestData(@Observes StartupEvent event) {
+        entityManager.createNativeQuery("DELETE FROM entry_tags").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM entry").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM category").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM tag").executeUpdate();
 
-        tagService.createTag(new Tag("Tag 1"));
-        tagService.createTag(new Tag("Tag 2"));
-        tagService.createTag(new Tag("Tag 3"));
+        // Categories
+        var projectACategory = new Category();
+        projectACategory.setTitle("Project A");
+        entityManager.persist(projectACategory);
 
-        entryService.createEntry(new Entry(LocalDateTime.now(), LocalDateTime.now(), category1));
+        var projectBCategory = new Category();
+        projectBCategory.setTitle("Project B");
+        entityManager.persist(projectBCategory);
+
+        var projectCCategory = new Category();
+        projectCCategory.setTitle("Project C");
+        entityManager.persist(projectCCategory);
+
+        // Tags
+        var programmingTag = new Tag();
+        programmingTag.setTitle("Programming");
+        entityManager.persist(programmingTag);
+
+        var debuggingTag = new Tag();
+        debuggingTag.setTitle("Debugging");
+        entityManager.persist(debuggingTag);
+
+        var meetingTag = new Tag();
+        meetingTag.setTitle("Meeting");
+        entityManager.persist(meetingTag);
+
+        // Entries
+        var firstEntry = new Entry();
+        firstEntry.setCategory(projectACategory);
+        firstEntry.setTags(new HashSet<>(Arrays.asList(programmingTag, debuggingTag)));
+        firstEntry.setCheckIn(LocalDateTime.now().minusHours(3));
+        firstEntry.setCheckOut(LocalDateTime.now().minusHours(2));
+        entityManager.persist(firstEntry);
+
+        var secondEntry = new Entry();
+        secondEntry.setCategory(projectACategory);
+        secondEntry.setTags(new HashSet<>(Arrays.asList(meetingTag)));
+        secondEntry.setCheckIn(LocalDateTime.now().minusHours(2));
+        secondEntry.setCheckOut(LocalDateTime.now().minusHours(1));
+        entityManager.persist(secondEntry);
+
+        var thirdEntry = new Entry();
+        thirdEntry.setCategory(projectBCategory);
+        thirdEntry.setTags(new HashSet<>(Arrays.asList(programmingTag)));
+        thirdEntry.setCheckIn(LocalDateTime.now().minusHours(1));
+        thirdEntry.setCheckOut(LocalDateTime.now());
+        entityManager.persist(thirdEntry);
     }
 
 }
